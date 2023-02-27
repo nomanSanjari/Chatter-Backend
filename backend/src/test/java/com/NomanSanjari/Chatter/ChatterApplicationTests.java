b874512b4b41e7f08186be58c49d1ebc5a603597
MD Noman Sanjari <nomansanjari2001@gmail.com>
2023-02-26 22:13:40 -0700

    Backend starter code generated using Spring Initializr

---
+++ backend/src/test/java/com/NomanSanjari/Chatter/ChatterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.NomanSanjari.Chatter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
