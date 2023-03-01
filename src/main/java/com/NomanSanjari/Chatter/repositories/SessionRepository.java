0247c63f96e8c431a843aec4c26dc5425387d20a
MD Noman Sanjari <nomansanjari2001@gmail.com>
2023-03-01 01:52:57 -0700

    I know what I did and what I have to do...starting some refactoring and testing

--- src/main/java/com/NomanSanjari/Chatter/repositories/SessionRepository.java
+++
@@ -1,8 +0,0 @@
-package com.NomanSanjari.Chatter.repositories;
-
-import com.NomanSanjari.Chatter.models.Session;
-import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
-
-public interface SessionRepository extends ReactiveMongoRepository<Session, String> {
-
-}
