34a9b1a93aaebd9eff8e9d3d010b1ae098c8f61b
MD Noman Sanjari <nomansanjari2001@gmail.com>
2023-03-01 01:44:55 -0700

    Some code refactoring...trying to understand exactly what I've done...

--- src/main/java/com/NomanSanjari/Chatter/controllers/UserController.java
+++ src/main/java/com/NomanSanjari/Chatter/controllers/UserController.java
@@ -26,13 +26,13 @@ public class UserController {
     /**
      * Creates a User document in the MongoDB database
      *
-     * @param user Data from request body, namely, username
+     * @param userData Data from request body, namely, username
      *
      * @return User object upon successful execution and HTTP code 201 CREATED
      */
     @PostMapping("/createUser")
-    public Mono<ResponseEntity<User>> createUser(@RequestBody User user) {
-        return userRepository.save(user)
+    public Mono<ResponseEntity<User>> createUser(@RequestBody User userData) {
+        return userRepository.save(userData)
                 .map(savedUser -> new ResponseEntity<>(savedUser, HttpStatus.CREATED));
     }
 
@@ -40,13 +40,13 @@ public class UserController {
      * Returns a User document from the MongoDB database
      * Uses User id stored in the browser upon creation to find the document
      *
-     * @param id Data from request path, namely, id of the user
+     * @param userID Data from request path, namely, id of the user
      *
      * @return User object upon successful execution HTTP code 302 FOUND
      */
     @GetMapping("/getUser/{id}")
-    public Mono<ResponseEntity<User>> getUser(@PathVariable String id) {
-        return userRepository.findById(id)
+    public Mono<ResponseEntity<User>> getUser(@PathVariable String userID) {
+        return userRepository.findById(userID)
                 .map(readUser -> new ResponseEntity<>(readUser, HttpStatus.FOUND))
                 .defaultIfEmpty(new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
@@ -54,17 +54,17 @@ public class UserController {
     /**
      * Updates attributes of a User document in the MongoDB database
      *
-     * @param id Data from request path, namely, id of the user
-     * @param user Data from request body, namely, new username
+     * @param userID Data from request path, namely, id of the user
+     * @param userData Data from request body, namely, new username
      *
      * @return Updated User object upon successful execution and HTTP code 200 OK
      */
     @PutMapping("/updateUser/{id}")
-    public Mono<ResponseEntity<User>> updateUser(@PathVariable String id, @RequestBody User user) {
-        return userRepository.findById(id)
+    public Mono<ResponseEntity<User>> updateUser(@PathVariable String userID, @RequestBody User userData) {
+        return userRepository.findById(userID)
                 .flatMap(existingUser -> {
-                    if (user.getUsername() != null) {
-                        existingUser.setUsername(user.getUsername());
+                    if (userData.getUsername() != null) {
+                        existingUser.setUsername(userData.getUsername());
                     }
                     return userRepository.save(existingUser);
                 })
@@ -75,13 +75,12 @@ public class UserController {
     /**
      * Deletes a User document from the MongoDB database
      *
-     * @param id Data from request path, namely, id of the user
+     * @param userID Data from request path, namely, id of the user
      * @return HttpStatus code, NO_CONTENT if success, NOT_FOUND if not successful
      */
     @DeleteMapping("/deleteUser/{id}")
-    public Mono<ResponseEntity<Object>> deleteUser(@PathVariable String id) {
-        return userRepository.findById(id)
-                .map(deletedUser -> new ResponseEntity<>(HttpStatus.NO_CONTENT))
-                .defaultIfEmpty(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    public Mono<ResponseEntity<Object>> deleteUser(@PathVariable String userID) {
+        return userRepository.deleteById(userID)
+                .map(deletedUser -> new ResponseEntity<>(HttpStatus.NO_CONTENT));
     }
 }
